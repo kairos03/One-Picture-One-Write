9dd9a95c3198aa86a913e37f79a858b97a95c62b
kairos03 <kairos9603@gmail.com>
2018-10-02 12:40:58 +0900

    add image upload

--- blog/models.py
+++ blog/models.py
@@ -1,10 +1,12 @@
 from django.db import models
 from django.utils import timezone
+from mysite import settings
 
 # Create your models here.
 class Post(models.Model):
     author = models.ForeignKey('auth.User', on_delete=models.CASCADE)
     title = models.CharField(max_length=200)
+    image = models.ImageField(upload_to="post/", default='None/no-image.jpg')
     text = models.TextField()
     created_date = models.DateTimeField(
             default=timezone.now)
--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,5 @@
+import os
+
 from django.shortcuts import render, get_object_or_404, redirect
 from django.utils import timezone
 
@@ -14,9 +16,10 @@ def post_detail(request, pk):
 
 def post_new(request):
     if request.method == "POST":
-        form = PostForm(request.POST)
+        form = PostForm(request.POST, request.FILES)
         if form.is_valid():
             post = form.save(commit=False)
+            post.image = form.cleaned_data['image']
             post.author = request.user
             post.published_date = timezone.now()
             post.save()
@@ -27,10 +30,14 @@ def post_new(request):
 
 def post_edit(request, pk):
     post = get_object_or_404(Post, pk=pk)
+    pre_image_path = post.image.path
     if request.method == "POST":
-        form = PostForm(request.POST, instance=post)
+        form = PostForm(request.POST, request.FILES, instance=post)
         if form.is_valid():
+            #delete pre uploaded image
+            _delete_file(pre_image_path)
             post = form.save(commit=False)
+            post.image = form.cleaned_data['image']
             post.author = request.user
             post.published_date = timezone.now()
             post.save()
@@ -38,3 +45,7 @@ def post_edit(request, pk):
     else:
         form = PostForm(instance=post)
     return render(request, 'blog/post_edit.html', {'form': form})
+
+def _delete_file(path):
+    if os.path.isfile(path):
+        os.remove(path)
